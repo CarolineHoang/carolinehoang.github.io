b4b62fb57beb44dca43c1c383656f892adf7aaaf
Cremebrutale <caroline.hoang@columbiaspectator.com>
2020-08-05 15:06:59 -0400

    Updates

---
+++ precache-manifest.a6058121db4abbc48da10dcb7f723c7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5724e0230e9125dd9a7402d98d802024",
+    "url": "/wsite2/index.html"
+  },
+  {
+    "revision": "6d721693454602b74483",
+    "url": "/wsite2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7dbedd7e8a9c7491a220",
+    "url": "/wsite2/static/js/2.414da3c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wsite2/static/js/2.414da3c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d721693454602b74483",
+    "url": "/wsite2/static/js/main.082344cf.chunk.js"
+  },
+  {
+    "revision": "5ec81121bf9a6984c426",
+    "url": "/wsite2/static/js/runtime-main.86e44d20.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wsite2/static/media/logo.5d5d9eef.svg"
+  }
+]);
