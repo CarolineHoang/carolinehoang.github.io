2faf17cf8e036aafc1356ce6c3be29b28da88dc9
Cremebrutale <caroline.hoang@columbiaspectator.com>
2020-08-14 11:44:38 -0400

    Updates

---
+++ precache-manifest.3ab8a5bb224d91197a3e1b76a650df4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3123036075f0b6464da5ead7428117da",
+    "url": "/old-personal-wsite/index.html"
+  },
+  {
+    "revision": "665ac472fcedf11a8b8f",
+    "url": "/old-personal-wsite/static/css/main.2c5c2e77.chunk.css"
+  },
+  {
+    "revision": "67371e8d705cd16b53bb",
+    "url": "/old-personal-wsite/static/js/2.d14a898f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/old-personal-wsite/static/js/2.d14a898f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "665ac472fcedf11a8b8f",
+    "url": "/old-personal-wsite/static/js/main.0ca99eff.chunk.js"
+  },
+  {
+    "revision": "84a0a6fe1c89ce54c284",
+    "url": "/old-personal-wsite/static/js/runtime-main.2b5125d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/old-personal-wsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0cc668ce30cc0fd303b2eec6a3f6d0fd.js"
+  "/old-personal-wsite/precache-manifest.3ab8a5bb224d91197a3e1b76a650df4a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/old-personal-wsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
