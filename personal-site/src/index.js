265cf3458019a9bd09cbbbde5eae7e859f017d87
CarolineHoang <caroline.hoang@columbiaspectator.com>
2025-01-07 02:14:22 -0500

    Updated dependencies

--- personal-site/src/index.js
+++ personal-site/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './App'; // This is the ID of the div in your index.html file
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+// 👇️ if you use TypeScript, add non-null (!) assertion operator
+// Note: this version doesn't so it's commented out
+// const root = createRoot(rootElement!);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </StrictMode>,
 );
 
 // If you want your app to work offline and load faster, you can change
